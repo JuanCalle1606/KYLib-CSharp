da5cb02f895262fb30db442f35080d045c9a5268
JuanCalle1606 <jpmilojp@gmail.com>
2021-01-26 20:32:53 -0500

    Clases Rand y ArrayExtensions

---
+++ Extensions/ArrayExtensions.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KYLib.Utils;
+
+namespace KYLib.Extensions
+{
+	public static class ArrayExtension
+	{
+		public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+		{
+			T[] elements = source.ToArray();
+			for (int i = elements.Length - 1; i > 0; i--)
+			{
+				int swapIndex = Rand.GetInt(i + 1);
+				yield return elements[swapIndex];
+				elements[swapIndex] = elements[i];
+			}
+			yield return elements[0];
+		}
+
+		public static int IndexOf<T>(this T[] arr, T target)
+		{
+			int dev = -1;
+			for (int i = 0; i < arr.Length; i++)
+				if (arr[i].Equals(target))
+					return i;
+			return dev;
+		}
+
+		public static void Sort<T>(this T[] arr, Comparison<T> comparison)
+		{
+			int len = arr.Length;
+			int sorted = 0;
+			int limit;
+			T aux;
+			while (sorted < len)
+			{
+				limit = len - 1 - sorted;
+				for (int i = 0; i < limit; i++)
+				{
+					if (comparison(arr[i], arr[i + 1]) > 0)
+					{
+						aux = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = aux;
+					}
+				}
+				sorted++;
+			}
+		}
+
+		public static void SortAsc<T>(this T[] arr) where T : IComparable<T> =>
+			arr.Sort((T1, T2) => T1.CompareTo(T2));
+
+		public static void SortDesc<T>(this T[] arr) where T : IComparable<T> =>
+			arr.Sort((T1, T2) => T1.CompareTo(T2) * -1);
+
+		public static int sum(this int[] arr)
+		{
+			int dev = 0;
+			foreach (int item in arr)
+				dev += item;
+			return dev;
+		}
+
+		public static int max(this int[] arr)
+		{
+			int dev = 0;
+			for (int i = 1; i < arr.Length; i++)
+				if (arr[i] > arr[dev])
+					dev = i;
+			return dev;
+		}
+
+		public static int min(this int[] arr)
+		{
+			int dev = 0;
+			for (int i = 1; i < arr.Length; i++)
+				if (arr[i] < arr[dev])
+					dev = i;
+			return dev;
+		}
+
+		public static T[][] ToBidimensionalArray<T>(this T[,] mat)
+		{
+
+			return null;
+		}
+
+		public static T[,] ToMatriz<T>(this T[][] arr)
+		{
+
+			return null;
+		}
+
+		public static int[] ToIntArray(this string[] arr)
+		{
+			int len = arr.Length;
+			int[] dev = new int[len];
+			for (int i = 0; i < len; i++)
+				dev[i] = int.Parse(arr[i]);
+
+			return dev;
+		}
+
+		public static float[] ToFloatArray(this string[] arr)
+		{
+			int len = arr.Length;
+			float[] dev = new float[len];
+			for (int i = 0; i < len; i++)
+				dev[i] = float.Parse(arr[i]);
+
+			return dev;
+		}
+
+		public static string ToString<T>(this T[] arr, char separator, bool ShowIndex)
+		{
+			string dev = "";
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (ShowIndex)
+					dev += $"{i}: ";
+				dev += arr[i].ToString() + separator;
+			}
+			dev = dev.TrimEnd(separator);
+			return dev;
+		}
+
+		public static string ToString<T>(this T[] arr, char separator) => arr.ToString(separator, false);
+
+		public static string ToString<T>(this T[][] arr, char separator, bool multiline, bool Showindex)
+		{
+			string dev = "";
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (Showindex)
+					dev += $"{i}: ";
+				dev += arr[i].ToString(separator);
+				if (multiline)
+					dev += "\n";
+			}
+			dev = dev.TrimEnd(separator);
+			return dev;
+		}
+
+		public static string ToString<T>(this T[][] arr, char separator, bool multiline) =>
+			arr.ToString(separator, multiline, false);
+
+		public static string ToString<T>(this T[,] arr, char separator, bool multiline, bool Showindex)
+		{
+			string dev = "";
+			for (int i = 0; i < arr.GetLength(0); i++)
+			{
+				if (Showindex)
+					dev += $"{i}: ";
+				for (int j = 0; j < arr.GetLength(1); j++)
+					dev += arr[i, j] + ",";
+				if (multiline)
+				{
+					dev = dev.TrimEnd(',');
+					dev += "\n";
+				}
+			}
+			dev = dev.TrimEnd(separator);
+			return dev;
+		}
+
+		public static string ToString<T>(this List<T> arr, char separator) =>
+			arr.ToArray().ToString(separator);
+	}
+}
---
+++ Utils/Rand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KYLib.Utils
+{
+	public static class Rand
+	{
+		private static Random random = new Random();
+
+		public static int GetInt() => random.Next();
+
+		public static int GetInt(int min, int max) => random.Next(min, max);
+
+		public static int GetInt(int max) => random.Next(max);
+
+		public static int[] GetInt(int min, int max, int amount)
+		{
+			if (!(amount > 0))
+				throw new ArgumentException();
+			int[] dev = new int[amount];
+			for (int i = 0; i < amount; i++)
+				dev[i] = GetInt(min, max);
+			return dev;
+		}
+
+		public static int[] GetInt(int min, int max, int minAmount, int maxAmout) => 
+			GetInt(min, max, GetInt(minAmount, maxAmout + 1));
+
+		public static float Get() => Convert.ToSingle(random.NextDouble());
+
+		public static float Get(float max) => Get() * max;
+
+		public static float Get(float min, float max) => (Get() * (max - min)) + min;
+
+		public static float[] Get(float min, float max, int amount)
+		{
+			if (!(amount > 0))
+				throw new ArgumentException();
+			float[] dev = new float[amount];
+			for (int i = 0; i < amount; i++)
+				dev[i] = Get(min, max);
+			return dev;
+		}
+
+		public static float[] Get(float min, float max, int minAmount, int maxAmout) =>
+			Get(min, max, GetInt(minAmount, maxAmout + 1));
+
+		public static float GetFixed(int decimals) => Fix(Get(), decimals);
+
+		public static float GetFixed(int decimals, float max) => Fix(Get() * max, decimals);
+
+		public static float GetFixed(int decimals, float min, float max) => Fix((Get() * (max - min)) + min, decimals);
+
+		public static float[] GetFixed(int decimals, float min, float max, int amount)
+		{
+			if (!(amount > 0))
+				throw new ArgumentException();
+			float[] dev = new float[amount];
+			for (int i = 0; i < amount; i++)
+				dev[i] = GetFixed(decimals, min, max);
+			return dev;
+		}
+
+		public static float[] GetFixed(int decimals, float min, float max, int minAmount, int maxAmout) =>
+			GetFixed(decimals, min, max, GetInt(minAmount, maxAmout + 1));
+
+		public static double GetDouble() => random.NextDouble();
+
+		public static T Choise<T>(T[] arr) => arr[random.Next(arr.Length)];
+
+		public static T Choise<T>(List<T> arr) => arr[random.Next(arr.Count)];
+
+		public static float Fix(float number, int decimals) => 
+			Convert.ToSingle(Math.Round(number, decimals));
+	}
+}
