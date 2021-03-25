365376b1875d5988607fa847e0c5aa2c21131f25
JuanCalle1606 <jpmilojp@gmail.com>
2021-03-24 21:33:52 -0500

    Cambios en Mathf

--- KYLib/MathFn/Mathf.cs
+++ KYLib/MathFn/Mathf.cs
@@ -15,17 +15,28 @@ namespace KYLib.MathFn
 		#region Utilidades
 
 		/// <summary>
-		/// Calcula la media de un conjunto de numeros enteros.
+		/// Calcula el porcentaje de <paramref name="n1"/> respecto a <paramref name="n2"/>.
 		/// </summary>
-		/// <param name="source">Conjunto de numeros de origen.</param>
-		/// <returns>Media del conjunto numerico.</returns>
-		public static int MeanOf(IEnumerable<int> source)
+		/// <param name="n1">Primer numero, este valor es la cantidad a evaluar.</param>
+		/// <param name="n2">Segundo numero, este valor es el limite, es decir, el 100%.</param>
+		/// <typeparam name="T">Tipo numerico que se retorna, este tipo debe ser especificado explitamente o se usara la sobrecarga que devuelve <see cref="Int"/>.</typeparam>
+		/// <returns>Devuelve el porcentaje de <paramref name="n1"/> con respecto a <paramref name="n2"/>.</returns>
+		public static T Percent<T>(INumber n1, INumber n2) where T : struct, INumber
 		{
-			int count = source.Count();
-			int sum = source.Sum();
-			sum /= count;
-			return sum;
+			Real div = n1.ToDouble(null) / n2.ToDouble(null);
+			T dev = new T();
+			dev.UpdateValue(div * 100);
+			return dev;
 		}
+
+		/// <summary>
+		/// Calcula el porcentaje de <paramref name="n1"/> respecto a <paramref name="n2"/>.
+		/// </summary>
+		/// <param name="n1">Primer numero, este valor es la cantidad a evaluar.</param>
+		/// <param name="n2">Segundo numero, este valor es el limite, es decir, el 100%.</param>
+		/// <returns>Devuelve el porcentaje de <paramref name="n1"/> con respecto a <paramref name="n2"/>.</returns>
+		public static Int Percent(INumber n1, INumber n2) =>
+			Percent<Int>(n1, n2);
 
 		/// <summary>
 		/// Calcula la media de un conjunto de numeros.
@@ -33,13 +44,21 @@ namespace KYLib.MathFn
 		/// <param name="source">Conjunto numerico.</param>
 		/// <typeparam name="T">Algun tipo numerico.</typeparam>
 		/// <returns>La media del conjunto de datos.</returns>
-		public static T MeanOf<T>(IEnumerable<T> source) where T : struct, INumber
+		public static T MeanOf<T>(IEnumerable<INumber> source) where T : struct, INumber
 		{
 			Int count = source.Count();
-			INumber sum = source.Sum();
+			INumber sum = source.Sum<T>();
 			sum.Div(count);
 			return (T)sum;
 		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		public static Real MeanOf(IEnumerable<INumber> source) =>
+			MeanOf<Real>(source);
 
 		/// <summary>
 		/// Calcula la media de un conjunto de numeros.
@@ -48,9 +67,29 @@ namespace KYLib.MathFn
 		/// <typeparam name="T">Algun tipo numerico.</typeparam>
 		/// <returns>La media del conjunto de datos.</returns>
 		public static T Mean<T>(params T[] source) where T : struct, INumber =>
-			MeanOf(source);
+			MeanOf<T>(source.ToArray<T, INumber>(i => i));
 
+		/// <summary>
+		/// Calcula la suma de los elementos de este arreglo.
+		/// </summary>
+		/// <param name="arr">Arreglo numerico de origen.</param>
+		/// <typeparam name="T">Cualquier tipo numerico.</typeparam>
+		/// <returns>Devuelve la suma de los elementos del arreglo.</returns>
+		public static T Sum<T>(IEnumerable<INumber> arr) where T : struct, INumber
+		{
+			T sum = default(T);
+			foreach (var item in arr)
+				sum.Add(item);
+			return sum;
+		}
 
+		/// <summary>
+		/// Calcula la suma de los elementos de este arreglo.
+		/// </summary>
+		/// <param name="arr">Arreglo numerico de origen.</param>
+		/// <returns>Devuelve la suma de los elementos del arreglo.</returns>
+		public static Real Sum(IEnumerable<INumber> arr) =>
+			arr.Sum<Real>();
 
 		#endregion
 
