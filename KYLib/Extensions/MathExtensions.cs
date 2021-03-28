328a55b33a945e1d12a848ffbe78cf6783bb0290
JuanCalle1606 <jpmilojp@gmail.com>
2021-03-27 20:51:27 -0500

    Agregadas extensiones matematicas

--- KYLib/Extensions/MathExtensions.cs
+++ KYLib/Extensions/MathExtensions.cs
@@ -14,17 +14,35 @@ namespace KYLib.Extensions
 		/// Calcula la suma de los elementos de este arreglo.
 		/// </summary>
 		/// <param name="arr">Arreglo numerico de origen.</param>
-		/// <typeparam name="T">Cualquier tipo numerico.</typeparam>
+		/// <typeparam name="T">Cualquier tipo numerico que se quiera por salida.</typeparam>
 		/// <returns>Devuelve la suma de los elementos del arreglo.</returns>
 		public static T Sum<T>(this IEnumerable<INumber> arr) where T : struct, INumber =>
-			Mathf.Sum<T>(arr);
+			Mathf.SumOf<T>(arr);
 
 		/// <summary>
 		/// Calcula la suma de los elementos de este arreglo.
 		/// </summary>
 		/// <param name="arr">Arreglo numerico de origen.</param>
 		/// <returns>Devuelve la suma de los elementos del arreglo.</returns>
-		public static Real Sum(this IEnumerable<INumber> arr) =>
-			Mathf.Sum(arr);
+		public static T Sum<T>(this IEnumerable<T> arr) where T : struct, INumber =>
+			Mathf.SumOf(arr);
+
+		/// <summary>
+		/// Calcula la media de este conjunto de numerico.
+		/// </summary>
+		/// <param name="arr">Arreglo numerico de origen.</param>
+		/// <typeparam name="T">Cualquier tipo numerico que se quiera por salida.</typeparam>
+		/// <returns>Devuelve la media del conjunto numerico.</returns>
+		public static T Mean<T>(this IEnumerable<INumber> arr) where T : struct, INumber =>
+			Mathf.MeanOf<T>(arr);
+
+		/// <summary>
+		/// Calcula la media de este conjunto de numerico.
+		/// </summary>
+		/// <param name="arr">Arreglo numerico de origen.</param>
+		/// <typeparam name="T">Cualquier tipo numerico que se quiera por salida.</typeparam>
+		/// <returns>Devuelve la media del conjunto numerico.</returns>
+		public static T Mean<T>(this IEnumerable<T> arr) where T : struct, INumber =>
+			Mathf.MeanOf(arr);
 	}
 }
