c95ea21d3381e064d9027aa55232e3bc200a9656
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-13 22:38:49 -0500

    Agregado el metodo Pow en Mathf

--- KYLib/MathFn/Math.cs
+++ KYLib/MathFn/Math.cs
@@ -58,9 +58,61 @@ namespace KYLib.MathFn
 		/// <returns>Devuelve la raiz <paramref name="nbase"/> positiva de <paramref name="num"/>. Si <paramref name="num"/> es un valor negativo se devolvera infinito o NaN.</returns>
 		public static T Root<T>(T num, T nbase) where T : struct, INumber =>
 			Root<T, T>(num, nbase);
+
+		#endregion Root
+
+		#region Pow
+
+		/// <summary>
+		/// Calcula la potencia <paramref name="nbase"/> de <paramref name="num"/>.
+		/// </summary>
+		/// <param name="num">Numero al que queremos calular la potencia.</param>
+		/// <param name="nbase">Base de la potencia, 2 es el cuadrado, 3 es el cubo, etc...</param>
+		/// <typeparam name="T">Tipo numerico que debe ser retornado, si no se especifica se retornara un <see cref="Real"/>.</typeparam>
+		/// <returns>Devuelve la potencia <paramref name="nbase"/> de <paramref name="num"/>.</returns>
+		public static T Pow<T>(INumber num, INumber nbase) where T : struct, INumber
+		{
+			var dev = default(T);
+			double rnum = num.ToDouble(null), rbase = nbase.ToDouble(null);
+
+			var math = Math.Pow(rnum, rbase);
+
+			dev.UpdateValue(math);
+			return dev;
+		}
+
+		/// <summary>
+		/// Calcula la potencia <paramref name="nbase"/> de <paramref name="num"/>.
+		/// </summary>
+		/// <param name="num">Numero al que queremos calular la potencia.</param>
+		/// <param name="nbase">Base de la potencia, 2 es el cuadrado, 3 es el cubo, etc...</param>
+		/// <returns>Devuelve la potencia <paramref name="nbase"/> de <paramref name="num"/>.</returns>
+		public static Real Pow(INumber num, INumber nbase) =>
+			Pow<Real>(num, nbase);
+
+		/// <summary>
+		/// Calcula la potencia <paramref name="nbase"/> de <paramref name="num"/>.
+		/// </summary>
+		/// <param name="num">Numero al que queremos calular la potencia.</param>
+		/// <param name="nbase">Base de la potencia, 2 es el cuadrado, 3 es el cubo, etc...</param>
+		/// <typeparam name="T">Tipo numerico de entrada, tanto el numero como la base deben ser del mismo tipo.</typeparam>
+		/// <typeparam name="TOut">Tipo numerico que debe ser retornado, si no se especifica se retornara un <see cref="Real"/>.</typeparam>
+		/// <returns>Devuelve la potencia <paramref name="nbase"/> de <paramref name="num"/>.</returns>
+		public static TOut Pow<T, TOut>(T num, T nbase)
+		where T : struct, INumber
+		where TOut : struct, INumber =>
+			Pow<TOut>(num, nbase);
+
+		/// <summary>
+		/// Calcula la potencia <paramref name="nbase"/> de <paramref name="num"/>.
+		/// </summary>
+		/// <param name="num">Numero al que queremos calular la potencia.</param>
+		/// <param name="nbase">Base de la potencia, 2 es el cuadrado, 3 es el cubo, etc...</param>
+		/// <typeparam name="T">Tipo numerico que se maneja, tanto el numero como la base deben ser del mismo tipo.</typeparam>
+		/// <returns>Devuelve la potencia <paramref name="nbase"/> de <paramref name="num"/>.</returns>
+		public static T Pow<T>(T num, T nbase) where T : struct, INumber =>
+			Pow<T, T>(num, nbase);
+
+		#endregion Pow
 	}
-
-	#endregion Root
-
-
 }
