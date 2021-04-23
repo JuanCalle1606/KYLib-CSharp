ffc18391c5a6ffb2230e626cf7637111efbf0910
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-23 12:02:14 -0500

    Terminada la clase Runner

--- KYLib/Utils/Runner.cs
+++ KYLib/Utils/Runner.cs
@@ -12,17 +12,17 @@ namespace KYLib.Utils
 		#region Every
 
 		/// <summary>
-		/// 
+		/// Ejecuta <paramref name="task"/> cada cierto tiempo.
 		/// </summary>
-		/// <param name="task"></param>
-		/// <param name="delay"></param>
-		/// <param name="repeats"></param>
+		/// <param name="task">Tarea a ejecutar repetidamente.</param>
+		/// <param name="delay">Tiempo de cada cuanto se eejcutara la función.</param>
+		/// <param name="repeats">Numero de veces que ejecutaremos <paramref name="task"/>.</param>
 		public static async void Every(Action task, TimeSpan delay, int repeats)
 		{
 			await Task.Run(async () =>
 			{
 				int exes = 0;
-				while (true && exes++ < repeats)
+				while (exes++ < repeats)
 				{
 					task?.Invoke();
 					await Task.Delay(delay);
@@ -31,11 +31,11 @@ namespace KYLib.Utils
 		}
 
 		/// <summary>
-		/// 
+		/// Ejecuta <paramref name="task"/> cada cierto tiempo.
 		/// </summary>
-		/// <param name="task"></param>
-		/// <param name="delay"></param>
-		/// <param name="token"></param>
+		/// <param name="task">Tarea a ejecutar repetidamente.</param>
+		/// <param name="delay">Tiempo de cada cuanto se ejecutara la función.</param>
+		/// <param name="token">Token de cancelación para cancelar la ejecución de la tarea.</param>
 		public static async void Every(Action task, TimeSpan delay, CancellationToken token)
 		{
 			await Task.Run(async () =>
@@ -47,13 +47,14 @@ namespace KYLib.Utils
 				}
 			});
 		}
+
 		/// <summary>
 		/// Ejecuta <paramref name="task"/> cada cierto tiempo.
 		/// </summary>
-		/// <param name="task"></param>
-		/// <param name="delay">Función a ejecutar</param>
-		/// <param name="predicate"></param>
-		public static async void Every(Action task, TimeSpan delay, Func<bool> predicate)
+		/// <param name="task">Tarea a ejecutar repetidamente.</param>
+		/// <param name="delay">Tiempo de cada cuanto se eejcutara la función.</param>
+		/// <param name="predicate">Función que se evalua para saber cuando detener la ejecución. Si evalua <c>false</c> se detiene la ejecución.</param>
+		public static async void Every(Action task, TimeSpan delay, Predicate predicate)
 		{
 			await Task.Run(async () =>
 			{
@@ -73,7 +74,7 @@ namespace KYLib.Utils
 		/// Detiene el ciclo actual y hasta que <paramref name="predicate"/> no evalue <c>false</c> no devuelve el control.
 		/// </summary>
 		/// <param name="predicate">Predicado que indica si se debe seguir esperando, si el predicado retorna <c>false</c> entonces se devuelve el control al llamador.</param>
-		public static void WaitWhile(Func<bool> predicate) =>
+		public static void WaitWhile(Predicate predicate) =>
 			WaitWhile(predicate, TimeSpan.FromMilliseconds(25));
 
 		/// <summary>
@@ -81,7 +82,7 @@ namespace KYLib.Utils
 		/// </summary>
 		/// <param name="predicate">Predicado que indica si se debe seguir esperando, si el predicado retorna <c>false</c> entonces se devuelve el control al llamador.</param>
 		/// <param name="interval">Intervalo de tiempo que indica cada cuando hay que evaluar <paramref name="predicate"/>.</param>
-		public static void WaitWhile(Func<bool> predicate, TimeSpan interval) =>
+		public static void WaitWhile(Predicate predicate, TimeSpan interval) =>
 			WaitWhile(predicate, interval, TimeSpan.Zero);
 
 		/// <summary>
@@ -90,7 +91,7 @@ namespace KYLib.Utils
 		/// <param name="predicate">Predicado que indica si se debe seguir esperando, si el predicado retorna <c>false</c> entonces se devuelve el control al llamador.</param>
 		/// <param name="interval">Intervalo de tiempo que indica cada cuando hay que evaluar <paramref name="predicate"/>.</param>
 		/// <param name="timeout">Maximo tiempo de espera, cuando se supera este tiempo entonces se devolvera el control. Si este valor es <see cref="TimeSpan.Zero"/> entonces se esperara indefinidamente.</param>
-		public static void WaitWhile(Func<bool> predicate, TimeSpan interval, TimeSpan timeout)
+		public static void WaitWhile(Predicate predicate, TimeSpan interval, TimeSpan timeout)
 		{
 			using (var token = timeout == TimeSpan.Zero ?
 			new CancellationTokenSource() :
@@ -107,4 +108,10 @@ namespace KYLib.Utils
 		#endregion
 
 	}
+
+	/// <summary>
+	/// Representa un metodo que se usa como bandera para saber si continuar o no con una tarea.
+	/// </summary>
+	/// <returns>Devuelve un valor que indica si continuar o no con la tarea.</returns>
+	public delegate bool Predicate();
 }
