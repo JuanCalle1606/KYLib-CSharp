18ad4eb5aa1d6ce61ae1071935a889779d2ec7a0
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-23 14:26:05 -0500

    Cambios en Assets

--- KYLib/Utils/Assets.cs
+++ KYLib/Utils/Assets.cs
@@ -14,6 +14,11 @@ namespace KYLib.Utils
 		public string Directory { get; private set; }
 
 		/// <summary>
+		/// Indica si las rutas devueltas por el indexador son absolutas.
+		/// </summary>
+		public bool ResolveAbsolute = false;
+
+		/// <summary>
 		/// Privatizamos el constructor sin parametros, esto no estara permitido.
 		/// </summary>
 		private Assets() { }
@@ -29,18 +34,26 @@ namespace KYLib.Utils
 		/// </summary>
 		/// <param name="path">Ruta relativa nueva</param>
 		public void UpdateRelPath(string path) =>
-			Directory = Path.Combine(Directory, path);
+			Directory = Path.GetFullPath(Path.GetRelativePath(Directory, path), Directory);
 
 		/// <summary>
-		/// Obtiene la ruta de <paramref name="filename"/> buscando en <see cref="Directory"/>.
+		/// Obtiene la ruta de <paramref name="filename"/> absoluta a <see cref="Directory"/>.
 		/// </summary>
 		/// <param name="filename">Nombre del archivo a buscar.</param>
 		public string GetPath(string filename) =>
-			Path.Combine(Directory, filename);
+			Path.GetFullPath(filename, Directory);
+
+		/// <summary>
+		/// Obtiene la ruta de <paramref name="filename"/> absoluta a <see cref="Directory"/> y relativa a <see cref="Info.CurrentDir"/>.
+		/// </summary>
+		/// <param name="filename">Nombre del archivo a buscar.</param>
+		public string GetRelPath(string filename) =>
+			Path.GetRelativePath(Info.CurrentDir, GetPath(filename));
 
 		/// <summary>
 		/// Obtiene un recurso relativo a esta instancia de <see cref="Assets"/>.
 		/// </summary>
-		public string this[string filename] => GetPath(filename);
+		public string this[string filename] => ResolveAbsolute ?
+			GetPath(filename) : GetRelPath(filename);
 	}
 }
