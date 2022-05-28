3910eed7449ca26bd70c36312181cd297e824172
JuanCalle1606 <jpmilojp@gmail.com>
2022-05-28 04:19:30 -0500

    Agregadas extenciones para procesos

---
+++ src/KYLib/Extensions/ProcessExtensions.cs
@@ -0,0 +1,48 @@
+﻿using KYLib.System;
+using KYLib.Utils;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace KYLib.Extensions;
+public static class ProcessExtensions
+{
+	public static string CommandLine(this Process process)
+	{
+		Ensure.NotNull(process, nameof(process));
+		if (Info.CurrentSystem.IsWindows())
+		{
+			IntPtr ptr;
+			try
+			{
+				ptr = process.Handle;
+			}
+			catch (Exception)
+			{
+				return string.Empty;
+			}
+
+
+			if (Windows.NtQueryInformationProcess(ptr,
+											Windows.PROCESSINFOCLASS.ProcessBasicInformation,
+											out Windows.ProcessBasicInformation pbi,
+											Marshal.SizeOf<Windows.ProcessBasicInformation>(),
+											out var _) != 0)
+				return string.Empty;
+			if (pbi.PebBaseAddress == IntPtr.Zero) return string.Empty;
+
+			if (Windows.ReadStructFromProcessMemory<Windows.PEB>(ptr, pbi.PebBaseAddress, out var peb))
+				if (Windows.ReadStructFromProcessMemory<Windows.RtlUserProcessParameters>(ptr, peb.ProcessParameters, out var rupp))
+					if (Windows.ReadUnicodeStringFromProccessMemory(ptr, rupp.CommandLine, out var cmd))
+						return cmd ?? string.Empty;
+		}
+		else if(Info.CurrentSystem.IsLinux())
+		{
+			return File.ReadAllText($"/proc/{process.Id}/cmdline");
+		}
+
+		return string.Empty;
+	}
+}
--- src/KYLib/System/Wraps.cs
+++ src/KYLib/System/Wraps.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using static KYLib.System.Windows;
+
 namespace KYLib.System;
 
 #pragma warning disable IDE1006
@@ -21,4 +25,119 @@ internal static class Windows
 	[DllImport("shell32.dll", SetLastError = true)]
 	[return: MarshalAs(UnmanagedType.Bool)]
 	internal static extern bool IsUserAnAdmin();
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct ProcessBasicInformation
+	{
+		public IntPtr Reserved1;
+		public IntPtr PebBaseAddress;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+		public IntPtr[] Reserved2;
+		public IntPtr UniqueProcessId;
+		public IntPtr Reserved3;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct UnicodeString
+	{
+		public ushort Length;
+		public ushort MaximumLength;
+		public IntPtr Buffer;
+	}
+
+	// This is not the real struct!
+	// I faked it to get ProcessParameters address.
+	// Actual struct definition:
+	// https://docs.microsoft.com/en-us/windows/win32/api/winternl/ns-winternl-peb
+	[StructLayout(LayoutKind.Sequential)]
+	public struct PEB
+	{
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+		public IntPtr[] Reserved;
+		public IntPtr ProcessParameters;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct RtlUserProcessParameters
+	{
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+		public byte[] Reserved1;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+		public IntPtr[] Reserved2;
+		public UnicodeString ImagePathName;
+		public UnicodeString CommandLine;
+	}
+
+	public enum PROCESSINFOCLASS
+	{
+		ProcessBasicInformation = 0x00,
+		ProcessDebugPort = 0x07,
+		ProcessExceptionPort = 0x08,
+		ProcessAccessToken = 0x09,
+		ProcessWow64Information = 0x1A,
+		ProcessImageFileName = 0x1B,
+		ProcessDebugObjectHandle = 0x1E,
+		ProcessDebugFlags = 0x1F,
+		ProcessExecuteFlags = 0x22,
+		ProcessInstrumentationCallback = 0x28,
+		MaxProcessInfoClass = 0x64
+	}
+
+	[DllImport("ntdll.dll")]
+	public static extern int NtQueryInformationProcess(IntPtr hProcess,
+													PROCESSINFOCLASS pic,
+													out ProcessBasicInformation pbi,
+													int cb,
+													out int pSize);
+
+
+	[DllImport("kernel32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	public static extern bool ReadProcessMemory(
+		IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer,
+		uint nSize, out uint lpNumberOfBytesRead);
+
+	public static bool ReadStructFromProcessMemory<TStruct>(
+		IntPtr hProcess, IntPtr lpBaseAddress, out TStruct? val)
+	{
+		val = default;
+		var structSize = Marshal.SizeOf<TStruct>();
+		var mem = Marshal.AllocHGlobal(structSize);
+		try
+		{
+			if (ReadProcessMemory(
+				hProcess, lpBaseAddress, mem, (uint)structSize, out var len) &&
+				(len == structSize))
+			{
+				val = Marshal.PtrToStructure<TStruct>(mem);
+				return true;
+			}
+		}
+		finally
+		{
+			Marshal.FreeHGlobal(mem);
+		}
+		return false;
+	}
+
+	public static bool ReadUnicodeStringFromProccessMemory(IntPtr hProcess, UnicodeString str, out string? val)
+	{
+		val = string.Empty;
+		var strLen = str.MaximumLength;
+		var mem = Marshal.AllocHGlobal(strLen);
+
+		try
+		{
+			if (ReadProcessMemory(hProcess, str.Buffer, mem, strLen, out var len))
+			{
+				val = Marshal.PtrToStringUni(mem);
+				return true;
+			}
+		}
+		finally
+		{
+			Marshal.FreeHGlobal(mem);
+		}
+		return false;
+	}
 }
